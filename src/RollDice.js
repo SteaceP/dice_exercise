2c61a815d0291d26d95afac3ef4382ee8e9c2e19
Steacy Paquette <steacy@live.ca>
2020-08-24 18:29:47 -0400

    first commit

---
+++ src/RollDice.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Die from "./Die";
+import "./RollDice.css";
+
+class RollDice extends Component {
+  static defaultProps = {
+    die: [
+      ["fas fa-dice-one"],
+      ["fas fa-dice-two"],
+      ["fas fa-dice-three"],
+      ["fas fa-dice-four"],
+      ["fas fa-dice-five"],
+      ["fas fa-dice-six"],
+    ],
+  };
+
+  state = { diceOne: 0, diceTwo: 0, anim: 1 };
+
+  handleRand = () => {
+    let rand1 = Math.floor(Math.random() * 6);
+    let rand2 = Math.floor(Math.random() * 6);
+    this.setState({ diceOne: rand1, diceTwo: rand2 });
+  };
+
+  handleAnim = () => {
+    this.setState({ anim: 1 });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="dice">
+          <Die face={[...this.props.die]} random={this.state.diceOne} />
+          <Die face={[...this.props.die]} random={this.state.diceTwo} />
+        </div>
+        <button
+          className="button"
+          onClick={this.handleAnim && this.handleRand}
+          onAnimationEnd={this.state.anim}
+        >
+          Roll Dice...
+        </button>
+      </>
+    );
+  }
+}
+
+export default RollDice;
